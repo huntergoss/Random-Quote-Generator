e8741077e912038760b1c599ca04408fc503891b
Hunter Goss <huntergoss1@hotmail.com>
2019-04-06 13:55:25 -0400

    Finished up project and added the extra credit features

--- js/script.js
+++ js/script.js
@@ -3,99 +3,129 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+  Array of objects that are game quotes that includes properties 
+  such as the quote, author, game, and year
 ***/
+
+var intervalId;
+
 var gameQuotes = [
   {
     quote: "We all make choices in life, but in the end our choices make us.",
     author: "Andrew Ryan",
     game: "BioShock",
-    year: "August 21, 2007"
+    year: "2007",
+    tags: "#fps #rpg"
   },
   {
     quote: "A hero need not speak. When he is gone, the world will speak for him.",
     author: "Master Chief",
     game: "Halo",
-    year: "November 15, 2001"
+    year: "2001",
+    tags: "#fps"
   },
   {
     quote: "It's time to kick ass and chew bubblegum... and I'm all outta gum.",
     author: "Duke Nukem",
     game: "Duke Nuke 3D",
-    year: "January 29, 1996"
+    year: "1996",
+    tags: "#fps"
   },
   {
     quote: "Don't wish it were easier, wish you were better.",
     author: "Chief",
     game: "Animal Crossing",
-    year: "April 14, 2001"
+    year: "2001",
+    tags: "sim"
   },
   {
     quote: "Good men mean well. We just don't always end up doing well.",
     author: "Isaac Clarke",
     game: "Dead Space",
-    year: "October 14, 2008"
+    year: "2008",
+    tags: "#rpg #survival"
   },
   {
     quote: "What is a man? A miserable little pile of secrets.",
     author: "Dracula",
     game: "Castlevania: Symphony of the Night",
-    year: "March 20, 1997"
+    year: "1997",
+    tags: "rpg"
   },
   {
     quote: "No matter how dark the night, morning always comes, and our journey begins anew.",
     author: "Lulu",
     game: "Final Fantasy X",
-    year: "July 19, 2001"
+    year: "2001",
+    tags: "#rpg"
   }
 ];
 
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
+  Returns a random quote object from our quote array above.
 ***/
+
 function getRandomQuote() {
-  var randomNumber;
+  var randomNumber = Math.floor(Math.random() * gameQuotes.length);
+  var randomQuote = gameQuotes[randomNumber];
+
+  return randomQuote;
+}
+
+/***
+ Returns a string value of randomly picked rgb values between 1-255 
+ ***/
+
+function randomColor() {
+  var randomRed = Math.floor(Math.random() * 256);
+  var randomGreen = Math.floor(Math.random() * 256);
+  var randomBlue = Math.floor(Math.random() * 256);
+
+  return 'rgb(' + randomRed + ',' + randomGreen + ',' + randomBlue + ')';
+}
+
+/***
+  This function is called by our button listener to present a new quote to the screen.
+  Uses getRandomQuote function to randomly pick a new quote from our array, then it
+  builds the html using the quotes property values. It will test to see if the quote has a game
+  name, year and tags before adding it to the new html string. Then it changes the quote-box 
+  element to the new html string
+***/
+
+function printQuote() {
+  var newQuote = getRandomQuote();
+  var html = '';
+
+  html += '<p class="quote">' + newQuote.quote + '</p>';
+  html += '<p class="source">' + newQuote.author;
+
+  if (newQuote.game) {
+    html += '<span class="citation">' + newQuote.game + '</span>';
+  }
+  if (newQuote.year) {
+    html += '<span class="year">' + newQuote.year + '</span>'
+  }
+  if (newQuote.tags) {
+    html += '<span class="tags">' + newQuote.tags + '</span>'
+  }
+  html += '</p>';
+
+  document.getElementById('quote-box').innerHTML = html;
+  document.body.style.backgroundColor = randomColor();
 }
 
 
+// This calls the printQuote function every 20 seconds
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
+intervalId = setInterval(printQuote, 20000);
 
 /***
   When the "Show another quote" button is clicked, the event listener 
   below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
+  function.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
